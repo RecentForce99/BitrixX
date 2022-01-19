6ef244d7d43ee9ec920c12dc103cb5f91862a85d
RecentFoce99 <ozornik.xd@mail.ru>
2022-01-20 02:23:54 +0300

    BitrixX

---
+++ moy-razdel/.left.menu.php
@@ -0,0 +1,11 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Новости", 
+		"/moy-razdel/news/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ moy-razdel/index.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Мой раздел");
+?><h1>Здесь что-то должно быть, вроде...</h1><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
