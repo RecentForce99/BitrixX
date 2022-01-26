d2408f62b12ebe95d745e29482c0b6e477e8dc59
RecentFoce99 <ozornik.xd@mail.ru>
2022-01-26 22:27:16 +0300

    BitrixX

--- lesson2/local/components — копия/ylab/cards.list/templates/.default/lang/ru/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-$MESS['YLAB.CARD.LIST.NUMBER'] = 'Номер карты:';
-$MESS['YLAB.CARD.LIST.USER'] = 'Владелец карты:';
-$MESS['YLAB.CARD.LIST.TYPE'] = 'Тип карты:';
-$MESS['YLAB.CARD.LIST.PRICE'] = 'Стоимость/месяц';
-$MESS['YLAB.CARD.LIST.SUM'] = 'Суммарная стоимость';
-$MESS['YLAB.CARD.LIST.TIME'] = 'Срок действия:';
-$MESS['YLAB.CARD.LIST.END'] = 'Дата окончания действия карты:';
-$MESS['YLAB.CARD.LIST.SECRET'] = 'Секретный ключ:';
